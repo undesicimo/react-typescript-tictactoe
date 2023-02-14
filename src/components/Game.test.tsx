45fdf145ba4a63c10a02bad087fbd37b51e7b833
Joshua Kawamata <email>
2023-02-14 15:59:22 +0900

    test: react testing libray test

---
+++ src/components/Game.test.tsx
@@ -0,0 +1,96 @@
+import { render, screen } from '@testing-library/react';
+import Game from './Game';
+import userEvent from '@testing-library/user-event';
+
+describe('Game component', () => {
+    it('should have history on clicks', async () => {
+        const user = userEvent.setup();
+        render(<Game />);
+        const squares = screen.getAllByRole('button');
+
+        await user.click(squares[0]);
+        await user.click(squares[1]);
+        await user.click(squares[2]);
+
+        // get all button with Go to move #~
+        const gameHistory = screen.getAllByText(/Go to move */);
+
+        expect(gameHistory).toHaveLength(3);
+    });
+
+    it('should go to first history move on click', async () => {
+        const user = userEvent.setup();
+        render(<Game />);
+        const squares = screen.getAllByRole('button');
+
+        await user.click(squares[0]);
+        await user.click(squares[1]);
+        await user.click(squares[2]);
+
+        const gameStartHistory = screen.getByText('Go to move #1');
+        await user.click(gameStartHistory);
+
+        expect(squares[0]).toHaveTextContent('X');
+    });
+});
+
+describe('Board component', () => {
+    it('should render default current move of X', () => {
+        render(<Game />);
+
+        const defaultPlayer = screen.getByText('Next player: X');
+
+        expect(defaultPlayer).toBeInTheDocument();
+    });
+
+    it('should show next player as O on click', async () => {
+        const user = userEvent.setup();
+        render(<Game />);
+
+        const squares = screen.getAllByRole('button');
+        await user.click(squares[0]);
+
+        const nextPlayer = screen.getByText('Next player: O');
+        expect(nextPlayer).toBeInTheDocument();
+    });
+
+    it('should show winner on winning condition', async () => {
+        const user = userEvent.setup();
+        render(<Game />);
+        const squares = screen.getAllByRole('button');
+
+        // Winner X
+        await user.click(squares[0]);
+        await user.click(squares[1]);
+        await user.click(squares[3]);
+        await user.click(squares[2]);
+        await user.click(squares[6]);
+
+        const winnerX = screen.getByText('Winner X');
+        expect(winnerX).toBeInTheDocument();
+    });
+});
+
+describe('Square component', () => {
+    it('should show X on button first move', async () => {
+        const user = userEvent.setup();
+        render(<Game />);
+        const squares = screen.getAllByRole('button');
+
+        await user.click(squares[0]);
+        await user.click(squares[0]);
+
+        expect(squares[0]).toHaveTextContent('X');
+    });
+
+    it('should show O on button on the second move', async () => {
+        const user = userEvent.setup();
+        render(<Game />);
+        const squares = screen.getAllByRole('button');
+
+        await user.click(squares[0]);
+        await user.click(squares[1]);
+
+        expect(squares[1]).toHaveTextContent('O');
+    });
+});
